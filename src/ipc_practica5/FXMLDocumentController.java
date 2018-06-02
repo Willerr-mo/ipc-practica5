b6b8226a08f73da0f5bfcd1cf8e21e9804929c25
Willerr-mo <wallysteam@hotmail.com>
2018-06-02 02:10:23 +0200

    jun2nd

--- src/ipc_practica5/FXMLDocumentController.java
+++ src/ipc_practica5/FXMLDocumentController.java
@@ -8,15 +8,19 @@ package ipc_practica5;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -28,9 +32,12 @@ public class FXMLDocumentController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private MenuBar myMenuBar;
 
-   
-    
+    @FXML
+    private MenuItem salir;
+
     @FXML
     private MenuItem amazonItem;
 
@@ -47,49 +54,69 @@ public class FXMLDocumentController implements Initializable {
     private MenuItem googleItem;
 
     @FXML
+    private CheckMenuItem amazonCheck;
+
+    @FXML
+    private CheckMenuItem ebayCheck;
+
+    @FXML
     private Button amazon;
 
     @FXML
-    private Button hola;
+    private Button blogger;
 
     @FXML
-    private Button hola1;
+    private Button ebay;
 
     @FXML
-    private Button hola2;
+    private Button facebook;
 
     @FXML
-    private Button hola3;
+    private Button googleplus;
 
     @FXML
     void buttonAction(ActionEvent event) {
-        System.out.println("holaaaaaaaaaaa");
+
+        String id = ((Node) event.getSource()).getId();
+
+        switch (id) {
+            case "amazon":
+                System.out.println("ousehgo");
+                break;
+
+            case "blogger":
+                break;
+
+            case "ebay":
+                break;
+
+            case "facebook":
+                break;
+
+            case "googleplus":
+                break;
+
+            default:
+                System.out.println("default");
+        }
+
     }
-    
-     @FXML
-    void menuAction(ActionEvent event) {
-         System.out.println("menuaction");
+
+    @FXML
+    void salir(ActionEvent event) {
+        Stage stage = (Stage) myMenuBar.getScene().getWindow();
+        stage.close();
     }
-    
+
     @FXML
     void menuLanzar(ActionEvent event) {
         System.out.println("lanzar");
     }
 
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        /** Image image = new Image(getClass().getResourceAsStream("amazonLogo.jpg"));
-         ImageView imageView = new ImageView();
-    
-       imageView.setImage(image);
-       amazon= new Button("oyoyoy", imageView);
-       amazon.setContentDisplay(ContentDisplay.TOP);
-        */
-    }    
+        
+        
+    }
 
-    
 }
-
-
